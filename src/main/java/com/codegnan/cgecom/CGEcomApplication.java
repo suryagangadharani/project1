6259e801503cc0270603231be859d394436a2a48
suryagangadharani <suryagangadharani4@gmail.com>
2025-12-31 19:06:33 +0530

    Initial Spring Boot WAR project

--- src/main/java/com/codegnan/cgecom/CGEcomApplication.java
+++ src/main/java/com/codegnan/cgecom/CGEcomApplication.java
@@ -2,16 +2,9 @@ package com.codegnan.cgecom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CGEcomApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(CGEcomApplication.class);
-    }
+public class CGEcomApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CGEcomApplication.class, args);
